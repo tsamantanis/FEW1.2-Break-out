ebe379a5845195357e1c487837ce0829b5d29552
tsamantanis <philip.tsamantanis@icloud.com>
2020-11-04 15:39:44 -0800

    Create Paddle class

---
+++ Paddle.js
@@ -0,0 +1,19 @@
+import TypeObject from './TypeObject';
+
+class Paddle extends TypeObject {
+  constructor(x, y, color, width, height) {
+    super(x, y, color);
+    this.width = width;
+    this.height = height;
+  }
+
+  render(ctx, canvas) {
+    ctx.beginPath();
+    ctx.rect(this.x, canvas.height - this.height, this.width, this.height);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+export default Paddle;
